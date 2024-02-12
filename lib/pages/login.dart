2ad25c18d3a063fbc7e23f82bb404bc11578e48c
asnamv08 <asnamvarghese001@gmail.com>
2024-02-12 15:31:14 +0530

    signup

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,5 +1,7 @@
 import 'package:blogapp/pages/signup.dart';
+import 'package:blogapp/services/blogservice.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class login extends StatefulWidget {
   const login({super.key});
@@ -9,6 +11,24 @@ class login extends StatefulWidget {
 }
 
 class _loginState extends State<login> {
+  TextEditingController getemail=new TextEditingController();
+  TextEditingController getpassword=new TextEditingController();
+  void sendvaluetoapi()async{
+    final response=await BlogApiService().loginapi(getemail.text,getpassword.text);
+    if (response["status"] == "success") {
+      String userId=response["userdata"]["_id"].toString();
+      SharedPreferences preferences=await SharedPreferences.getInstance();
+      preferences.setString("userId", userId);
+      print("Login Successfully"+userId);
+
+    }
+    else if (response["status"] == "invalid user") {
+      print("invalid user");
+    }
+    else{
+      print("Invalid Password");
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/services/blogservice.dart
+++ lib/services/blogservice.dart
@@ -4,6 +4,30 @@ import 'package:http/http.dart' as http;
 import 'package:blogapp/models/blogmodel.dart';
 
 class  BlogApiService{
+  Future<dynamic>loginapi(
+      String email,
+      String password,
+      ) async{
+    var client=http.Client();
+    var apiurl=Uri.parse("");
+    var response=await client.post(apiurl,
+      headers: <String,String>{
+        "Content-Type":"application/json ; charset=UTF-8"
+      },
+        body:jsonEncode(<String,String>
+        {
+          "email": email,
+          "password": password,
+        })
+    );
+    if(response.statusCode==200){
+      return jsonDecode(response.body);
+
+    }
+    else{
+      throw Exception("failed to send data");
+    }
+  }
   Future<dynamic>senddata(
   String name,
   String age,
